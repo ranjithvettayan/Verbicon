46338393df9ea55673b99c5c6a751e36307a8fc9
daredevil <daredevil@daredevil>
2024-09-22 21:57:51 +0530

    verbicon first commit

---
+++ neurocraft-ai-image-forge/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import NeuroCraftImageForge from './NeuroCraftImageForge';
+
+function App() {
+  return (
+    <div className="App">
+      <NeuroCraftImageForge />
+    </div>
+  );
+}
+
+export default App;
